f4a714e910ce3d9b6b3ca17438ee55a5ea22beda
Yağızcan Şahin <85365132+Yagizcan00@users.noreply.github.com>
2023-04-26 09:34:51 +0300

    Update

--- myapp/src/components/Content/PageTactic.jsx
+++ myapp/src/components/Content/PageTactic.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 
 import ContentContext from "../../context/ContentContext";
 
@@ -10,9 +10,36 @@ import Draggable from "react-draggable";
 
 export default function PageTactic() {
 
-    const { check, activeFormation, setActiveFormation, checkFormation, setCheckFormation, } = useContext(ContentContext)
+
+    // States coming from Context file.
+    const {
+        check,
+        activeFormation,
+        setActiveFormation,
+        checkFormation,
+        setCheckFormation,
+        colorOne,
+        setColorOne,
+        colorTwo,
+        setColorTwo,
+        checkColorOne,
+        setCheckColorOne,
+        checkColorTwo,
+        setCheckColorTwo,
+        backgroundColorOne,
+        setBackgroundColorOne,
+        backgroundColorTwo,
+        setBackgroundColorTwo,
+        textColorOne,
+        setTextColorOne,
+        textColorTwo,
+        setTextColorTwo,
+    } = useContext(ContentContext)
 
 
+
+
+    // Active and Inactive div styles.
     const divStyleActive = {
         marginLeft: "20vw",
         transition: "all 0.75s ease",
@@ -25,8 +52,17 @@ export default function PageTactic() {
 
         width: "88vw",
     }
+    // Active or Inactive situation of Dropdown Menu. 
+    const dropdownStyleActive = {
+        display: "flex",
+        flexDirection: "column",
+    }
+    const dropdownStyleInactive = {
+        display: "none"
+    }
 
 
+    // Formations array.
     const formations = [
         "5-0-1",
         "4-0-2",
@@ -42,26 +78,139 @@ export default function PageTactic() {
         "1-1-4",
         "1-0-5",
     ]
-    
-
-    const formationClassname = ""
-    const changeFormationClassname = () => {
-        
-    }
-
-
-    const dropdownStyleActive = {
-        display: "flex",
-        flexDirection: "column",
-    }
-    const dropdownStyleInactive = {
-        display: "none"
-    }
-
-
+    // Function for change the active formation.
     function handleFormationClick(formation) {
         setActiveFormation(formation);
     }
+
+
+
+
+    // Color array.
+    const colors = [
+        "Beyaz",
+        "Gri",
+        "Kırmızı",
+        "Mavi",
+        "Mor",
+        "Pembe",
+        "Sarı",
+        "Siyah",
+        "Turuncu",
+        "Yeşil",
+    ]
+    // Functions for change colors selected of user.
+    function handleColorOneClick(colorOne) {
+        setColorOne(colorOne);
+
+        if (colorOne === "Beyaz") {
+
+            setBackgroundColorOne("white")
+            setTextColorOne("black")
+
+        } else if (colorOne === "Gri") {
+
+            setBackgroundColorOne("grey")
+            setTextColorOne("white")
+
+        } else if (colorOne === "Kırmızı") {
+
+            setBackgroundColorOne("rgb(200, 0, 0)")
+            setTextColorOne("white")
+
+        } else if (colorOne === "Mavi") {
+
+            setBackgroundColorOne("blue")
+            setTextColorOne("white")
+
+        } else if (colorOne === "Mor") {
+
+            setBackgroundColorOne("purple")
+            setTextColorOne("white")
+
+        } else if (colorOne === "Pembe") {
+
+            setBackgroundColorOne("#FF1493")
+            setTextColorOne("white")
+
+        } else if (colorOne === "Sarı") {
+
+            setBackgroundColorOne("yellow")
+            setTextColorOne("black")
+
+        } else if (colorOne === "Siyah") {
+
+            setBackgroundColorOne("black")
+            setTextColorOne("white")
+
+        } else if (colorOne === "Turuncu") {
+
+            setBackgroundColorOne("#FF5F15")
+            setTextColorOne("white")
+
+        } else if (colorOne === "Yeşil") {
+
+            setBackgroundColorOne("green")
+            setTextColorOne("white")
+
+        }
+    }
+    function handleColorTwoClick(colorTwo) {
+        setColorTwo(colorTwo);
+
+        if (colorTwo === "Beyaz") {
+
+            setBackgroundColorTwo("white")
+            setTextColorTwo("black")
+
+        } else if (colorTwo === "Gri") {
+
+            setBackgroundColorTwo("grey")
+            setTextColorTwo("white")
+
+        } else if (colorTwo === "Kırmızı") {
+
+            setBackgroundColorTwo("red")
+            setTextColorTwo("white")
+
+        } else if (colorTwo === "Mavi") {
+
+            setBackgroundColorTwo("blue")
+            setTextColorTwo("white")
+
+        } else if (colorTwo === "Mor") {
+
+            setBackgroundColorTwo("purple")
+            setTextColorTwo("white")
+
+        } else if (colorTwo === "Pembe") {
+
+            setBackgroundColorTwo("#FF1493")
+            setTextColorTwo("white")
+
+        } else if (colorTwo === "Sarı") {
+
+            setBackgroundColorTwo("yellow")
+            setTextColorTwo("black")
+
+        } else if (colorTwo === "Siyah") {
+
+            setBackgroundColorTwo("black")
+            setTextColorTwo("white")
+
+        } else if (colorTwo === "Turuncu") {
+
+            setBackgroundColorTwo("#FF5F15")
+            setTextColorTwo("white")
+
+        } else if (colorTwo === "Yeşil") {
+
+            setBackgroundColorTwo("green")
+            setTextColorTwo("white")
+
+        }
+    }
+
 
 
     return (
@@ -77,7 +226,7 @@ export default function PageTactic() {
                     <div>
                         <button onClick={() => setCheckFormation(!checkFormation)}>{activeFormation}</button>
 
-                        <div className="dropdown" style={checkFormation ? dropdownStyleActive : dropdownStyleInactive}>
+                        <div className="dropdownTactics" style={checkFormation ? dropdownStyleActive : dropdownStyleInactive}>
                             {
                                 formations.map((item) => (
                                     <p key={item} onClick={() => handleFormationClick(item) & setCheckFormation(!checkFormation)}>
@@ -90,9 +239,38 @@ export default function PageTactic() {
 
                 </div>
 
-                {/* Player number selection part */}
-                <div>
-                            
+                {/* Player color selection part */}
+                <div className="listColors">
+
+                    <h1>Renkler :</h1>
+
+                    <div className="colorOne">
+                        <button onClick={() => setCheckColorOne(!checkColorOne)} style={{ backgroundColor: backgroundColorOne, color: textColorOne }}>{colorOne}</button>
+
+                        <div className="dropdownColorOne" style={checkColorOne ? dropdownStyleActive : dropdownStyleInactive}>
+                            {
+                                colors.map((item) => (
+                                    <p key={item} onClick={() => handleColorOneClick(item) & setCheckColorOne(!checkColorOne)}>
+                                        {item}
+                                    </p>
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    <div className="colorTwo">
+                        <button onClick={() => setCheckColorTwo(!checkColorTwo)} style={{ backgroundColor: backgroundColorTwo, color: textColorTwo }}>{colorTwo}</button>
+
+                        <div className="dropdownColorTwo" style={checkColorTwo ? dropdownStyleActive : dropdownStyleInactive}>
+                            {
+                                colors.map((item) => (
+                                    <p key={item} onClick={() => handleColorTwoClick(item) & setCheckColorTwo(!checkColorTwo)}>
+                                        {item}
+                                    </p>
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
 
             </div>
@@ -102,43 +280,43 @@ export default function PageTactic() {
                 {/* Players */}
 
                 <Draggable>
-                    <section className="playerOne3-2-1">
+                    <section className={"playerOne" + activeFormation} style={{ background: `linear-gradient(90deg, ${backgroundColorOne} 50%, ${backgroundColorTwo} 50%)` }}>
                         <h1>1</h1>
                     </section>
                 </Draggable>
 
                 <Draggable>
-                    <section className="playerTwo3-2-1">
+                    <section className={"playerTwo" + activeFormation} style={{ background: `linear-gradient(90deg, ${backgroundColorOne} 50%, ${backgroundColorTwo} 50%)` }}>
                         <h1>2</h1>
                     </section>
                 </Draggable>
 
                 <Draggable>
-                    <section className="playerThree3-2-1">
+                    <section className={"playerThree" + activeFormation} style={{ background: `linear-gradient(90deg, ${backgroundColorOne} 50%, ${backgroundColorTwo} 50%)` }}>
                         <h1>3</h1>
                     </section>
                 </Draggable>
 
                 <Draggable>
-                    <section className="playerFour3-2-1">
+                    <section className={"playerFour" + activeFormation} style={{ background: `linear-gradient(90deg, ${backgroundColorOne} 50%, ${backgroundColorTwo} 50%)` }}>
                         <h1>4</h1>
                     </section>
                 </Draggable>
 
                 <Draggable>
-                    <section className="playerFive3-2-1">
+                    <section className={"playerFive" + activeFormation} style={{ background: `linear-gradient(90deg, ${backgroundColorOne} 50%, ${backgroundColorTwo} 50%)` }}>
                         <h1>5</h1>
                     </section>
                 </Draggable>
 
                 <Draggable>
-                    <section className="playerSix3-2-1">
+                    <section className={"playerSix" + activeFormation} style={{ background: `linear-gradient(90deg, ${backgroundColorOne} 50%, ${backgroundColorTwo} 50%)` }}>
                         <h1>6</h1>
                     </section>
                 </Draggable>
 
                 <Draggable>
-                    <section className="playerSeven3-2-1">
+                    <section className={"playerSeven" + activeFormation} style={{ background: `linear-gradient(90deg, ${backgroundColorOne} 50%, ${backgroundColorTwo} 50%)` }}>
                         <h1>7</h1>
                     </section>
                 </Draggable>
--- myapp/src/context/ContentContext.jsx
+++ myapp/src/context/ContentContext.jsx
@@ -15,13 +15,45 @@ export const ContentProvider = ({ children }) => {
     const [checkFormation, setCheckFormation] = useState(false)
 
 
+    const [colorOne, setColorOne] = useState("Kırmızı")
+
+
+    const [colorTwo, setColorTwo] = useState("Beyaz")
+
+
+    const [checkColorOne, setCheckColorOne] = useState(false)
+
+
+    const [checkColorTwo, setCheckColorTwo] = useState(false)
+
+    const [backgroundColorOne, setBackgroundColorOne] = useState("rgb(200, 0, 0)")
+    const [backgroundColorTwo, setBackgroundColorTwo] = useState("White")
+    const [textColorOne, setTextColorOne] = useState("white")
+    const [textColorTwo, setTextColorTwo] = useState("black")
+
     const values = {
         check,
         setCheck,
         activeFormation,
         setActiveFormation,
-        checkFormation, 
+        checkFormation,
         setCheckFormation,
+        colorOne,
+        setColorOne,
+        colorTwo,
+        setColorTwo,
+        checkColorOne,
+        setCheckColorOne,
+        checkColorTwo,
+        setCheckColorTwo,
+        backgroundColorOne,
+        setBackgroundColorOne,
+        backgroundColorTwo,
+        setBackgroundColorTwo,
+        textColorOne,
+        setTextColorOne,
+        textColorTwo,
+        setTextColorTwo,
     }
 
     return <ContentContext.Provider value={values}> {children} </ContentContext.Provider>
